248292125e82b806d0e4936c1c30b3c190403a8a
Kenneth Tambuwun <31935680+kennethnyu@users.noreply.github.com>
2024-12-12 21:10:54 -0500

    du

--- src/data/General.py
+++ src/data/General.py
@@ -32,6 +32,7 @@ class Config(object):
                         "Miamieater - EUC", "Miamidab - EUC",
                         "Unreflective - NAW", "Lacerative - NAW", "Precipitative - NAW", "Obliterative - NAW",   
                         "Fnd - EUC", "Fndtwo - EUC", "Fndthree - EUC", "Fndfour - EUC", "Fndfive - EUC", "Fndsix - EUC",
+                        "Antigóne - NAE", "Cooletrain - NAE", "Lyriaoflagos - NAE", "Vívienné - NAE"
                          ]
         # Spec to color dictionary
         self.spec_to_color={"Berserker Technique":"#EE2E48","Mayhem":"#EE2E48",
